f3e48c5087b3c6bc4c438ec5682979824e28a5bf
ernestovv85 <ernestovv85@gmail.com>
2023-12-02 11:50:29 -0600

    Exercise 1 solved

---
+++ src/basic/exercises/Exercise1.java
@@ -0,0 +1,15 @@
+package basic.exercises;
+/*Declara dos variables numéricas (cualquier valor),
+* muestra por consola la suma, la resta, la multiplicación,
+* la división y el módulo*/
+public class Exercise1 {
+    public static void exercise1() {
+        int num1 = 4, num2 = 3;
+
+        System.out.println("La suma es: " + (num1 + num2));
+        System.out.println("La resta es: " + (num1 - num2));
+        System.out.println("La multiplicación es: " + (num1 * num2));
+        System.out.println("La división es: " + (num1/num2));
+        System.out.println("El módulo es: " + (num1 % num2));
+    }
+}
