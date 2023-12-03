1630792e043355fd37ecaec02495938dd15abc4d
ernestovv85 <ernestovv85@gmail.com>
2023-12-03 14:49:11 -0600

    Mid level package added

---
+++ src/mid/Main.java
@@ -0,0 +1,7 @@
+package mid;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/mid/classes/Cookie.java
@@ -0,0 +1,24 @@
+package mid.classes;
+
+/*Estructura básica de una clase*/
+public class Cookie {
+    // Atributos
+    String type, form, flavor, color;
+
+    //Constructor
+    public Cookie() {
+    }
+
+    //Métodos
+    void recipe1() {
+
+    }
+
+    void recipe2() {
+
+    }
+
+    void recipe3() {
+
+    }
+}
